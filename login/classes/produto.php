4635d181861b5b31346f04df83a76354efbf7e75
Luis <luisfernandoalvesdesouza0123@gmail.com>
2022-05-06 20:26:54 -0300

    Tela de Estabelecimento e Cardápio ok

--- login/classes/produto.php
+++ login/classes/produto.php
@@ -17,32 +17,40 @@ Class Produto
         }
     }
 
-    public function cadastrar($nome, $imagem, $preco, $descricao, $codigo, $id_estabelecimento)
+    public function cadastrar($nome, $imagem, $preco, $descricao, $id_estabelecimento)
     {
         global $pdo;
-        //verificar se já existe email cadastrado
-        $sql = $pdo->prepare("SELECT id FROM produtos WHERE codigo = :c");
-        $sql->bindValue(":c",$codigo);
+        $sql = $pdo->prepare("INSERT INTO produtos (nome, imagem, preco, descricao, id_estabelecimento) VALUES (:n, :i, :p, :d, :id)");
+        $sql->bindValue(":n",$nome);
+        $sql->bindValue(":i",$imagem);
+        $sql->bindValue(":p",$preco);     
+        $sql->bindValue(":d",$descricao);
+        $sql->bindValue(":id",$id_estabelecimento);
         $sql->execute();
-        if($sql->rowCount() > 0)
-        {
-            return false;
-        }
-        else
-        { //cadastrar produto não cadastrado
-            $sql = $pdo->prepare("INSERT INTO produtos (nome, imagem, preco, descricao, codigo, id) VALUES (:n, :i, :p, :d, :c, :id)");
-            $sql->bindValue(":n",$nome);
-            $sql->bindValue(":i",$imagem);
-            $sql->bindValue(":p",$preco);     
-            $sql->bindValue(":d",$descricao);
-            $sql->bindValue(":c",$codigo);
-            $sql->bindValue(":id",$id_estabelecimento);
-            $sql->execute();
-            return true;
-        }        
+        return true;
     }
-
-
+    public function editar($nome, $imagem, $preco, $descricao, $id, $id_estabelecimento)
+    {
+        global $pdo;
+        $sql = $pdo->prepare("UPDATE produtos SET nome = :n, imagem = :i, preco = :p, descricao = :d WHERE id = :id and id_estabelecimento = :id_estabelecimento");
+        $sql->bindValue(":n",$nome);
+        $sql->bindValue(":i",$imagem);
+        $sql->bindValue(":p",$preco);     
+        $sql->bindValue(":d",$descricao);
+        $sql->bindValue(":id",$id);
+        $sql->bindValue(":id_estabelecimento",$id_estabelecimento);
+        $sql->execute();
+        return true;
+    }
+    public function deletar($id, $id_estabelecimento)
+    {
+        global $pdo;
+        $sql = $pdo->prepare("DELETE from produtos WHERE id = :id and id_estabelecimento = :id_estabelecimento");
+        $sql->bindValue(":id",$id);
+        $sql->bindValue(":id_estabelecimento",$id_estabelecimento);
+        $sql->execute();
+        return true;
+    }
 }
 
 ?>
